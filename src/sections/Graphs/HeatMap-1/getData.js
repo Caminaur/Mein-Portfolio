a4f6d6a9f862989d6da38d0e119388a673b686a2
Julian <jacaminaur@gmail.com>
2025-12-20 12:13:29 +0100

    URL for API request attemp Nr 9.123.559

--- src/sections/Graphs/HeatMap-1/getData.js
+++ src/sections/Graphs/HeatMap-1/getData.js
@@ -2,7 +2,8 @@ import { heatmapByHourRanges } from "./heatmapByHourRanges";
 
 export const getData = async (t) => {
   const API_URL = import.meta.env.VITE_API_URL;
-  const res = await fetch(`${API_URL}/engineering-habits`);
+  const res = await fetch(`${API_URL}/api/engineering-habits`);
+
   const data = await res.json();
   const updatedLabel = new Date(data.computedAt).toLocaleString("De");
   return [
